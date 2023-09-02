fff03d3dd5be238bce0ebe85ea532e93cdfc1f15
asommacal <sommacalalberto@gmail.com>
2023-09-02 21:26:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let homeEl = document.getElementById("homeScore")
+let guestEl = document.getElementById("guestScore")
+
+
+let homeScore = 0
+let guestScore = 0
+
+function home1() {
+    homeScore += 1
+    console.log(homeScore)
+    homeEl.textContent = homeScore  
+}
+
+function home2() {
+    homeScore += 2
+    homeEl.textContent = homeScore
+}
+
+function home3() {
+    homeScore += 3
+    homeEl.textContent = homeScore
+}
+
+function guest1() {
+    guestScore += 1
+    guestEl.textContent = guestScore
+}
+
+function guest2() {
+    guestScore += 2
+    guestEl.textContent = guestScore
+}
+
+function guest3() {
+    guestScore += 3
+    guestEl.textContent = guestScore
+}
+
